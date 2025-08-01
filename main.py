5ec11c7357c691698560f1f0140d37b62eb9aa6b
Craig <craig@oppkey.com>
2025-08-01 09:30:49 -0700

    working without auth

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.templating import Jinja2Templates
 from sqladmin import Admin, ModelView
 from db import async_engine
 from models import User
-from users import fastapi_users, auth_backend  # type: ignore
+# from users import fastapi_users, auth_backend  # type: ignore
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -18,8 +18,8 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Welcome to FastAPI Admin Demo"})
 
 # Type warnings are expected with FastAPI Users async setup
-app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])  # type: ignore
-app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])  # type: ignore
+# app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])  # type: ignore
+# app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])  # type: ignore
 
 admin = Admin(app, async_engine)
 
